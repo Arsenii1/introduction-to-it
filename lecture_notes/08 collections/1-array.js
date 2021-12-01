0d837dfee64fd0fbc3cb15184b30ce1059fa4ec5
Arsenii1 <94102472+Arsenii1@users.noreply.github.com>
2021-12-01 18:51:17 +0200

    commit

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
-
+ages.splice(4, 1)
+ages.unshift(8)
+ages.push(21)
 const first = ages[0];
 const last = ages[ages.length - 1];
 
--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -1,5 +1,9 @@
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 
+del ages[-1]
+ages.insert(0,9)
+ages.append(21)
+
 first = ages[0]
 last = ages[-1]
 
