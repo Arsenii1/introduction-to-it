cc965fe2e38b5223f9e3fa267145b9a4b6aca6ba
Arsenii1 <94102472+Arsenii1@users.noreply.github.com>
2021-12-03 10:08:37 +0200

    changed from 6-set.js to py

---
+++ lecture_notes/08 collections/6-set.py
@@ -0,0 +1,10 @@
+ages = {10, 12, 15, 15, 17, 18, 18, 19, 20}
+ages.remove(20)
+ages.add(16)
+print(ages)
+
+age = 10 in ages
+age1 = 16 in ages
+age2 = 19 in ages
+age3 = 20 in ages
+print(age, age1, age2, age3)
